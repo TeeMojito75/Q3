bb49edf9ca430bf2493b6a6ccbf7e06551502788
Joan Carles <iesm.joancarles.veny@gmail.com>
2022-11-23 18:10:48 +0100

    Previa lab so y juego ya fucniona

--- eda/Joc/P31294_ca(1)/game/AIProva.cc
+++ eda/Joc/P31294_ca(1)/game/AIProva.cc
@@ -30,7 +30,7 @@ struct PLAYER_NAME : public Player {
     //Devuelve true si la en la casilla hay enemigo o un zombie
     bool z_o_e(int fila, int col) {
       int id = cell(fila, col).id;
-      if (id != -1 and unit(id).type != Dead and (unit(id).type == Zombie or (unit(id).type == Alive and cell(fila, col).owner != me()))) return true;
+      if (id != -1 and unit(id).type != Dead and (unit(id).type == Zombie or (unit(id).type == Alive and unit(id).player != me()))) return true;
       return false;
     }
 
@@ -38,42 +38,43 @@ struct PLAYER_NAME : public Player {
     bool moviment_inicial(int id, Dir& d) {
       Pos aux = unit(id).pos;
       int f = aux.i, c = aux.j;
-      if (c-1 > 0 and cell(f, c-1).type != Waste and (cell(f, c-1).food or z_o_e(f, c-1))) {d = Left; return true;}
-      if (c+1 < 60 and cell(f, c+1).type != Waste and (cell(f, c+1).food or z_o_e(f, c+1))) {d = Right; return true;}
-      if (f-1 > 0 and cell(f-1, c).type != Waste and (cell(f-1, c).food or z_o_e(f-1, c))) {d = Up; return true;}
-      if (f+1 < 60 and cell(f+1, c).type != Waste and (cell(f+1, c).food or z_o_e(f+1, c))) {d = Down; return true;}
+      if (c-1 >= 0 and cell(f, c-1).type != Waste and (cell(f, c-1).food or z_o_e(f, c-1))) {d = Left; return true;}
+      if (c+1 < board_cols() and cell(f, c+1).type != Waste and (cell(f, c+1).food or z_o_e(f, c+1))) {d = Right; return true;}
+      if (f-1 >= 0 and cell(f-1, c).type != Waste and (cell(f-1, c).food or z_o_e(f-1, c))) {d = Up; return true;}
+      if (f+1 < board_rows() and cell(f+1, c).type != Waste and (cell(f+1, c).food or z_o_e(f+1, c))) {d = Down; return true;}
       return false;
     }
     
-    bool bfs_general(int id, Dir& d) {
+    bool bfs_menjar(int id, Dir& d) {
       Pos aux = unit(id).pos;
       int f = aux.i, c = aux.j;
-      VVB visit(60, vector<bool>(60, false));
+      VVB visit(board_rows(), vector<bool>(board_cols(), false));
       queue<pair<Pos, Dir>> Q;
       visit[f][c] = true;
 
       //Primero comprobamos si las posiciones adyacentes tienen algo, en caso contrario miramos más profundidad        
-      if (c-1 > 0 and not visit[f][c-1] and cell(f, c-1).type != Waste) {
-          if (cell(f, c-1).food or z_o_e(f, c-1)) {d = Left; return true;}
+      if (c-1 >= 0 and not visit[f][c-1] and cell(f, c-1).type != Waste) {
+          if (cell(f, c-1).food) {d = Left; return true;}
           else Q.push(make_pair(aux, Left));
       }
 
-      if (c+1 < 60 and not visit[f][c+1] and cell(f, c+1).type != Waste) {
-          if (cell(f, c+1).food or z_o_e(f, c+1)) {d = Right; return true;}
+      if (c+1 < board_cols() and not visit[f][c+1] and cell(f, c+1).type != Waste) {
+          if (cell(f, c+1).food) {d = Right; return true;}
           else Q.push(make_pair(aux, Right));
       }
       
-      if (f-1 > 0 and not visit[f-1][c] and cell(f-1, c).type != Waste) {
-          if (cell(f-1, c).food or z_o_e(f-1, c)) {d = Up; return true;}
+      if (f-1 >= 0 and not visit[f-1][c] and cell(f-1, c).type != Waste) {
+          if (cell(f-1, c).food) {d = Up; return true;}
           else Q.push(make_pair(aux, Up));
       }
 
-      if (f+1 < 60 and not visit[f+1][c] and cell(f+1, c).type != Waste) {
-          if (cell(f+1, c).food or z_o_e(f+1, c)) {d = Down; return true;}
+      if (f+1 < board_rows() and not visit[f+1][c] and cell(f+1, c).type != Waste) {
+          if (cell(f+1, c).food) {d = Down; return true;}
           else Q.push(make_pair(aux, Down));
       }
 
-      while (not Q.empty()) {
+      int n = 0;
+      while (not Q.empty() and n <= 20) {
         Dir de = Q.front().second;
         Pos p = Q.front().first + de;
         f = p.i, c = p.j;
@@ -81,35 +82,92 @@ struct PLAYER_NAME : public Player {
         visit[f][c] = true;
       
       if (c-1 >= 0 and not visit[f][c-1] and cell(f, c-1).type != Waste) {
-        if (cell(f, c-1).food or z_o_e(f, c-1)) {d = Left; return true;}
-        else Q.push(make_pair(aux, Left));
+        if (cell(f, c-1).food) {d = Left; return true;}
+        else Q.push(make_pair(p, Left));
       }
 
-      if (c+1 < 60 and not visit[f][c+1] and cell(f, c+1).type != Waste) {
-        if (cell(f, c+1).food or z_o_e(f, c+1)) {d = Right; return true;} 
-        else Q.push(make_pair(aux, Right));
+      if (c+1 < board_cols() and not visit[f][c+1] and cell(f, c+1).type != Waste) {
+        if (cell(f, c+1).food) {d = Right; return true;} 
+        else Q.push(make_pair(p, Right));
       }
       
       if (f-1 >= 0 and not visit[f-1][c] and cell(f-1, c).type != Waste) {
-        if (cell(f-1, c).food or z_o_e(f-1, c)) {d = Up; return true;}
-        else Q.push(make_pair(aux, Up));
+        if (cell(f-1, c).food) {d = Up; return true;}
+        else Q.push(make_pair(p, Up));
       }
 
-      if (f+1 < 60 and not visit[f+1][c] and cell(f+1, c).type != Waste) {
-        if (cell(f+1, c).food or z_o_e(f+1, c)) {d = Down; return true;}
-        else Q.push(make_pair(aux, Down));
+      if (f+1 < board_rows() and not visit[f+1][c] and cell(f+1, c).type != Waste) {
+        if (cell(f+1, c).food) {d = Down; return true;}
+        else Q.push(make_pair(p, Down));
       }
-       
+       ++n;
       }
       return false;
     }
 
+    const vector<Dir> dirs = {Left, Right, Up, Down};
 
-  /**
-   * Play method, invoked once per each round.
-   */
-  virtual void play () {
-    // Getting the alive units
+    int bfs_atacar(int id) {
+      Pos aux = unit(id).pos;
+      int f = aux.i, c = aux.j;
+      VVB visit(board_cols(), vector<bool>(board_rows(), false));
+      queue<pair<Pos, Dir>> Q;
+      visit[f][c] = true;
+
+      //Primero comprobamos si las posiciones adyacentes tienen algo, en caso contrario miramos más profundidad        
+      if (c-1 >= 0 and not visit[f][c-1] and cell(f, c-1).type != Waste) {
+          if (z_o_e(f, c-1)) return 0;
+          else Q.push(make_pair(aux, Left));
+      }
+
+      if (c+1 < board_cols() and not visit[f][c+1] and cell(f, c+1).type != Waste) {
+          if (z_o_e(f, c+1)) return 1;
+          else Q.push(make_pair(aux, Right));
+      }
+      
+      if (f-1 >= 0 and not visit[f-1][c] and cell(f-1, c).type != Waste) {
+          if (z_o_e(f-1, c)) return 2;
+          else Q.push(make_pair(aux, Up));
+      }
+
+      if (f+1 < board_rows() and not visit[f+1][c] and cell(f+1, c).type != Waste) {
+          if (z_o_e(f+1, c)) return 3;
+          else Q.push(make_pair(aux, Down));
+      }
+
+      int n = 0;
+      while (not Q.empty() and n <= 15) {
+        Dir de = Q.front().second;
+        Pos p = Q.front().first + de;
+        f = p.i, c = p.j;
+        Q.pop();
+        visit[f][c] = true;
+      
+      if (c-1 >= 0 and not visit[f][c-1] and cell(f, c-1).type != Waste) {
+        if (z_o_e(f, c-1)) return 0;
+        else Q.push(make_pair(p, Left));
+      }
+
+      if (c+1 < board_cols() and not visit[f][c+1] and cell(f, c+1).type != Waste) {
+        if (z_o_e(f, c+1)) return 1; 
+        else Q.push(make_pair(p, Right));
+      }
+      
+      if (f-1 >= 0 and not visit[f-1][c] and cell(f-1, c).type != Waste) {
+        if (z_o_e(f-1, c)) return 2; 
+        else Q.push(make_pair(p, Up));
+      }
+
+      if (f+1 < board_rows() and not visit[f+1][c] and cell(f+1, c).type != Waste) {
+        if (z_o_e(f+1, c)) return 3;
+        else Q.push(make_pair(p, Down));
+      }
+       ++n;
+      }
+      return -1;
+    }
+
+  void moure_peces() {
     vector<int> alive = alive_units(me());
      
       for (auto id : alive) {
@@ -117,11 +175,27 @@ struct PLAYER_NAME : public Player {
         bool mov = moviment_inicial(id, moviment);
         if (mov) move(id, moviment);
         else {
-          bool mov2 = false;
-          mov2 = bfs_general(id, moviment);
-          if (mov2) move(id, moviment);
+          bool mv = bfs_menjar(id, moviment);
+          if (mv) move(id, moviment);
+          else {
+              int xy = bfs_atacar(id);
+              if (xy != -1) move(id, dirs[xy]);
+              else {
+                Dir d = dirs[random(0,dirs.size()-1)];
+	              Pos new_pos = unit(id).pos + d;
+	              if (pos_ok(new_pos) and cell(new_pos.i,new_pos.j).type != Waste) move(id,d);
+              }
+          }
         }
       }
+  }
+
+  /**
+   * Play method, invoked once per each round.
+   */
+  virtual void play () {
+    // Getting the alive units
+    moure_peces();
   }
 };
 
